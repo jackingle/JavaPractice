3aab17ee37fac830417ea2cc22dd258590611ceb
Jack Ingle <jackingle@outlook.com>
2019-12-11 12:22:51 -0600

    Add MultiThread practice class

--- src/MultiThreadPalindrome.java
+++ src/MultiThreadPalindrome.java
@@ -1,125 +1,154 @@
+import java.util.HashMap;
 import java.util.Vector;
 
 public class MultiThreadPalindrome implements Runnable {
+	static HashMap<MultiThreadPalindrome, Integer> threadIDMap = new HashMap<MultiThreadPalindrome, Integer>();
+	static Vector<Boolean> checkVector = new Vector<Boolean>();
+	static public ThreadGroup group = new ThreadGroup("PalCheck");
+	int threadID;
+	static int divisions = 10;
+	static int threadCount = divisions /2;
+	static Thread finalThread;
+	static int finishedThreadCount = 0;
 	
 	public static void main(String[] args) {
-		MultiThreadPalindrome obj = new MultiThreadPalindrome();
+	
+		for(int i = 1; i < threadCount + 1; i++) {
+			MultiThreadPalindrome obj = new MultiThreadPalindrome();
+			obj.threadID = i;
+			Thread process = new Thread(obj);
+			process.start();
+			if (i == threadCount) {
+				finalThread = process;
+				}
+			}
+			
+			
+			//System.out.println(group.toString());
+	//		threadIDMap.put(obj, 1);
+	//		threadIDMap.put(obj1, 2);
+	//		threadIDMap.put(obj2,  3);
+	//		threadIDMap.put(obj3, 4);
+	//		processAlpha.start();
+	//		processBeta.start();
+	//		processSigma.start();
+	//		processDelta.setPriority(4);
+	//		try {
+	//			processDelta.sleep(1000);
+	//		} catch (InterruptedException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		}
+	//		processDelta.start();
+			
+			
+			
+		}
+	synchronized static void threadCount() {
+		finishedThreadCount++;
 		
-		
-		System.out.println(obj.size);
-		System.out.println(obj.quarter);
-		System.out.println(obj.half);
 	}
-	Character[] palindrome = new Character[] {'t', 'o', 'y', 'o', 't', 'a', 'r', 'a', 'c', 'e', 'c', 'a', 'r', 'a', 't', 'o', 'y', 'o', 't'};
-	Thread processAlpha = new Thread();
-	Thread processBeta = new Thread();
+	static Character[] palindrome = new Character[] {'t', 'o', 'y', 'o', 't', 'a', 'r', 'a', 'c', 'e', 'c', 'a', 'r', 'a', 't', 'o', 'y', 'o', 't'};
+	
+	static Character[] newPalindrome = convertString(makeString());
+	
+	static String makeString() {
+		StringBuilder newString = new StringBuilder(); 
+		for(int i = 0; i < 1000000; i++) {
+			newString.append("aaaaaaaaaaa");
+		} 
+		
+		return newString.toString();
+	}
+	
+	static Vector<Character> palVector1 = newVector(newPalindrome);
 	int size = palindrome.length;
 	int quarter = size / 4;
 	int half = size / 2;
-	
-	
-public static Vector<Character> newVector(Character[] thisVector) {
+	static Vector<Character>  palVector = newVector(palindrome);
 		
-	
-		Vector<Character> nVect = new Vector<Character>();
-		for(int i = 0; i < thisVector.length; i++) {
-			nVect.add(thisVector[i]);
+	public static Character[] convertString(String name) {
+		
+		Character[] arr = new Character[name.length()];
+		for(int i = 0; i < name.length(); i++) {
+			arr[i] = name.charAt(i);
 		}
-		
-	return nVect;
+		return arr;
 	}
-
-public boolean oddCheckOutsideQuarters(Vector<Character> thisVector) {
-	int j = thisVector.size();
-	int segmentLength = thisVector.size() / 4;
-	//i goes from 0 to 3rd index, j goes from 19 to 16th index
-	for(int i = 0; i < (segmentLength); i++) {
 		
-		if (thisVector.get(i) == thisVector.get(j)) {
+		
+	public static Vector<Character> newVector(Character[] thisVector) {
 			
-			j--;
-			if(i == segmentLength && thisVector.get(segmentLength) == thisVector.get(j)) {
-				return true;
+		
+			Vector<Character> nVect = new Vector<Character>();
+			for(int i = 0; i < thisVector.length; i++) {
+				nVect.add(thisVector[i]);
 			}
-		}	
+			
+		return nVect;
+		}
+	
+	//19, 1, 4
+	public int checkSegmentStartPosition(int vectorLength, int threadID, int divisions) {
+		int numOfOperations = vectorLength / divisions; // 4
+		int startIndex = (threadID - 1) * numOfOperations; // 0
+		// 3 - 1 * 4 == 8
+		//i == 8 
+		return startIndex;
 	}
 	
-	
-	return false;
-}
-public boolean oddCheckInsideQuarters(Vector<Character> thisVector) {
-	int segmentLength = thisVector.size() / 4;
-	int j = thisVector.size() - segmentLength;
-	
-	//i goes from 4th to 7th index, j goes from 15th to 11th index
-	for(int i = segmentLength; i < segmentLength * 2; i++) {
+	public boolean checkQuadrant(Vector<Character> thisVector, int threadID, int divisions) {
 		
-		if (thisVector.get(i) == thisVector.get(j)) {
+		int startIndex = checkSegmentStartPosition(thisVector.size(), threadID, divisions);
+		
+		for(int i = startIndex; i < startIndex + (thisVector.size() / divisions) && i < thisVector.size() / 2; i++) {
 			
-			j--;
-			if(i == segmentLength * 2 - 1 && thisVector.get((segmentLength * 2) - 1) == thisVector.get(j)) {
-				return true;
+			if(thisVector.get(i) != thisVector.get(thisVector.size() - i - 1)) {
+				return false;
 			}
 		}
+		
+		return true;
 	}
+	static boolean check = true;
 	
-	return false;
-}
-public boolean oddCheckLastQuarter(Vector<Character> thisVector) {
-	int segmentLength = thisVector.size() / 4;
-	int j = thisVector.size() - segmentLength * 2;
-	//i is 8, j is 10
-	for(int i = segmentLength * 2; i < thisVector.size() / 2; i++) {
-		j--;
-		
-		if(i = segmentLength * 2 && )
-		
-	}
+	@Override
+	public void run() {
 	
-	
-	
-	return false;
-	
-	
-}
-
-public boolean threadCheck(Vector<Character> thisVector, int pos) {
-	
-	
-	
-	return false;
-}
-
-
-//19, 1, 4
-public int checkSegmentStartPosition(int vectorLength, int threadID, int divisions) {
-	int numOfOperations = vectorLength / divisions; // 4
-	int startIndex = (threadID - 1) * numOfOperations; // 0
-	// 3 - 1 * 4 == 8
-	//i == 8 
-	return startIndex;
-}
-
-public boolean checkQuadrant(Vector<Character> thisVector, int threadID, int divisions) {
-	
-	int startIndex = checkSegmentStartPosition(thisVector.size(), threadID, divisions);
-	
-	for(int i = startIndex; i < startIndex + (thisVector.size() / divisions) && i < thisVector.size() / 2; i++) {
-		
-		if(i == thisVector.size() - i && thisVector.get(i) == thisVector.get(thisVector.size() - i)) {
-			return true;
+//		System.out.println(this.threadID);
+//		System.out.println("Initialized"+threadID);
+//		//System.out.println(checkQuadrant(palVector, threadID, 4));
+		if(!checkQuadrant(palVector1, threadID, divisions)) {
+			check = false;
 		}
+		//checkVector.add(checkQuadrant(palVector1, threadID, divisions));
+		
+		
+		
+//		System.out.println(checkVector);
+//		System.out.println(checkVector.size());
+		
+		finishedThreadCount++;
+		System.out.println(finishedThreadCount);
+		if(finishedThreadCount == threadCount) {
+			System.out.println(check);
+			System.out.println(finishedThreadCount);
+		}
+		//System.out.println("Finished"+threadID);
+		
+		
+		
+	}
+		
+		
 	}
 	
-	return false;
-}
-
-
-@Override
-public void run() {
-	// TODO Auto-generated method stub
-	
-}
 	
 	
-}
+	
+	
+	
+	
+	
+	
+	
